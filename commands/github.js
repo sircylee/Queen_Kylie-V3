563cb7eb566619bfbe62bbc34bfd9ef6902cbfd6
Cadillac Cylee <kylie.x.cylee.technology@gmail.com>
2025-04-12 22:54:29 +0200

    Update github.js

--- commands/github.js
+++ commands/github.js
@@ -1,11 +1,11 @@
 async function githubCommand(sock, chatId) {
-    const repoInfo = `*🤖 KnightBot MD*
+    const repoInfo = `*QUEEN_KYLIE-V3👸❤️*
 
 *📂 GitHub Repository:*
-https://github.com/mruniquehacker/Knightbot-MD
+https://github.com/sircylee/Queen_Kylie-V3
 
 *📢 Official Channel:*
-https://youtube.com/@mr_unique_hacker
+https://youtube.com/@sircylee999
 
 _Star ⭐ the repository if you like the bot!_`;
 
@@ -17,7 +17,7 @@ _Star ⭐ the repository if you like the bot!_`;
                 isForwarded: true,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '120363161513685998@newsletter',
-                    newsletterName: 'KnightBot MD',
+                    newsletterName: 'Queen_Kylie-V3',
                     serverMessageId: -1
                 }
             }
